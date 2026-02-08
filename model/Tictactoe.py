aafc38aac75f5603d559c6b8a1d14e124a3eb4f5
T3chpriest <t3chpriest@proton.me>
2026-02-08 10:12:13 +0300

    dev3: implemented random choice for pc turn, 123 removed

--- model/Tictactoe.py
+++ model/Tictactoe.py
@@ -1,3 +1,6 @@
+import random
+from random import choice
+
 class Cell:
     def __init__(self, value = 0):
         self.value = value
@@ -16,15 +19,39 @@ class TicTacToe:
         self.__pole = tuple([tuple([Cell() for _ in range(self.SIZE)]) for _ in range(self.SIZE)])
 
     @staticmethod
-    def index_check(idx):
+    def __index_check(idx):
         max_index = 2
         min_index = 0
-        if not type(idx) is int or idx < min_index or idx > max_index:
-            raise IndexError('некорректно указанные индексы')
+        for _ in idx:
+            if not type(_) is int or _ < min_index or _ > max_index:
+                raise IndexError('некорректно указанные индексы')
 
 
     def __setitem__(self, key, value):
+        self.__index_check(key)
         i, j = key
         self.__pole[i][j] = value
+
+
+    def __getitem__(self, key):
+        self.__index_check(key)
+        i, j = key
+        return self.__pole[i][j]
+
+
+    def show(self):
+        print(self.__pole)
+
+
+    def human_go(self):
+        self[input()] = self.HUMAN_X
+
+
+    def computer_go(self):
+        self[random.choice(self.__scan_field())] = self.COMPUTER_O
+
+
+    def __scan_field(self):
+        return [i for j in self.__pole for i in j if i]
 
 
